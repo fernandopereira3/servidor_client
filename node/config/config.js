4d36a3bae4a3f9d0a9d97582ed33ab82aa309548
Fernando <fernandopereira-3@hotmail.com>
2023-12-02 09:47:58 -0300

    remocao string db

--- node/config/config.js
+++ node/config/config.js
@@ -4,18 +4,18 @@ const config = () => {
     switch (env){
     case 'net':
     return{ 
-        db_string:'mongodb+srv://admin_fernando:umbrella@usuario0-0yvr5.azure.mongodb.net/test?retryWrites=true&w=majority',
-        jwt_password: 'linguica2020'
+        db_string:'',
+        jwt_password: ' '
     }
     case 'loc':
     return {
-        db_string:'geonosis.mongodb.umbler.com:36887',
-        jwt_password: 'linguica2020'
+        db_string:' ',
+        jwt_password: ' '
     }
     
     case 'prod':
     return {
-        db_string:'mongodb+srv://admin_fernando:umbrella@usuario0-0yvr5.azure.mongodb.net/test?retryWrites=true&w=majority'
+        db_string:' '
 
     }
     }
